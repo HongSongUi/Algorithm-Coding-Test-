0fb8ab849e311118c4c8261911c601ed418e8789
thddml <108908383+HongSongUi@users.noreply.github.com>
2024-04-07 20:02:30 +0900

    [Bronze I] Title: 팰린드롬수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+		int n = 0;
+	int half = 0;
+	string temp = "";
+	int index = 0;
+	int rindex = 0;
+	int count = 0;
+	while (true)
+	{
+		cin >> n;
+		if (n == 0)
+		{
+			break;
+		}
+		temp = to_string(n);
+		rindex = temp.length() - 1;
+		half = temp.length() / 2;
+		if (half % 2 != 0)
+		{
+			half++;
+		}
+		while (index < half)
+		{
+			if (temp[index] != temp[rindex])
+			{
+				cout << "no" << endl;
+				break;
+			}
+			else
+			{
+				count++;
+			}
+			index++;
+			rindex--;
+		}
+		if (count == half)
+		{
+			cout << "yes"<<endl;
+		}
+		count = 0;
+		index = 0;
+	}
+	return 0;
+}	
