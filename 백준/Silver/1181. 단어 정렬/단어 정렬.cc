9a1ae3a204ef293cfd28cbb8194d74edf4c21012
thddml <108908383+HongSongUi@users.noreply.github.com>
2024-04-07 22:17:52 +0900

    [Silver V] Title: 단어 정렬, Time: 996 ms, Memory: 3848 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+bool Compair(string a, string b)
+{
+	if (a.length() == b.length())
+	{
+		return a < b;
+	}
+	else
+	{
+		return a.length() < b.length();
+	}
+}
+int main()
+{
+	int count = 0;
+	string temp = "";
+	cin >> count;
+	vector<string> words(count);
+	for (int i = 0; i < count; i++)
+	{
+		cin >> temp;
+		words[i] = temp;
+	}
+	sort(words.begin(), words.end(), Compair);
+	for (int i = 0; i < count; i++)
+	{
+		if (i != 0)
+		{
+			if (words[i] == words[i - 1])
+			{
+				continue;
+			}
+		}
+		cout << words[i] << endl;
+	}
+}
