aec61a261fcf7ee1592ab88fd85993fb04c3a865
thddml <108908383+HongSongUi@users.noreply.github.com>
2024-04-11 20:59:29 +0900

    [Silver V] Title: 덩치, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.cc
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Stat 
+{
+	int index;
+	int height;
+	int weight;
+	int rank;
+	Stat()
+	{
+		index = 0;
+		height = 0;
+		weight = 0;
+		rank = 0;
+	}
+	void SetRank(int r)
+	{
+		rank = r;
+	}
+	bool operator== (Stat& s)
+	{
+		if (this->index == s.index)
+		{
+			return true;
+		}
+		return false;
+	}
+	bool operator>(Stat& s)
+	{
+		if ((this->height > s.height) && (this->weight > s.weight))
+		{
+			return true;
+		}
+		return false;
+	}
+};
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int num = 0;
+	cin >> num;
+	int rank = 0;
+	vector<Stat> buff(num);
+
+	for (int i = 0; i < num; i++)
+	{
+		Stat s;
+		cin >> s.weight >> s.height;
+		s.index = i;
+		buff[i] = s;
+	}
+	for (int i = 0; i < num; i++)
+	{
+		Stat temp = buff[i];
+		rank = 0;
+		for (int j = 0; j < num; j++)
+		{
+			if (temp == buff[j])
+			{
+				continue;
+			}
+			if (buff[j] >temp)
+			{
+				rank++;
+			}
+		}
+		buff[i].rank = rank + 1;
+	}
+	for (int i = 0; i < num; i++)
+	{
+		cout << buff[i].rank << " ";
+	}
+}
