1a3bd7b64beaba13f096cb20be14a33162049da5
thddml <108908383+HongSongUi@users.noreply.github.com>
2024-04-16 01:06:07 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,31 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int Find(int n , vector<int>& buff)
-{
-	if (n == 1)
-	{
-		return 0;
-	}
-	if (buff[n] != -1)
-	{
-		return buff[n];
-	}
-
-	int result = Find(n - 1, buff)+1;
-
-	if (n % 3 == 0)
-	{
-		result = min(Find(n / 3, buff) + 1, result) ;
-	}
-	if (n % 2 == 0)
-	{
-		result = min(Find(n / 2, buff) + 1, result);
-	}
-	buff[n] = result;
-	return result;
-}
-
 int main()
 {
 	ios::sync_with_stdio(false);
@@ -34,7 +9,19 @@ int main()
 	
 	int n = 0;
 	cin >> n;
-	vector<int> buff(1000001, -1);
-
-	cout<< Find(n, buff);
+	vector<int> buff(n+1, 0);
+	buff[1] = 0;
+	for (int i = 2; i <= n; i++)
+	{
+		buff[i] = buff[i - 1] + 1;
+		if (i % 3 == 0)
+		{
+			buff[i] = min(buff[i / 3] + 1, buff[i]);
+		}
+		if (i % 2 == 0)
+		{
+			buff[i] = min(buff[i / 2] + 1, buff[i]);
+		}
+	}
+	cout << buff[n];
 }
