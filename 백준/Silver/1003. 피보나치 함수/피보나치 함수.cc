efd91e08e0606e70911f79c3c9bac21ee8caec19
thddml <108908383+HongSongUi@users.noreply.github.com>
2024-04-15 20:48:21 +0900

    [Silver III] Title: 피보나치 함수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -1,26 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Buffer
-{
-	int zero_count;
-	int one_count;
-	Buffer()
-	{
-		zero_count = 0;
-		one_count = 0;
-	}
-	Buffer(int z, int o)
-	{
-		zero_count = z;
-		one_count = o;
-	}
-	Buffer operator+ (Buffer& a)
-	{
-		return Buffer(zero_count + a.zero_count, one_count + a.one_count);
-	}
-};
-
 int main()
 {
 	ios::sync_with_stdio(false);
@@ -32,16 +12,19 @@ int main()
 	cin >> n;
 	int zero_count = 0;
 	int one_count = 0;
-	vector<Buffer> buff(41);
-	buff[0] = Buffer(1, 0);
-	buff[1] = Buffer(0, 1);
+	vector<vector<int>> buff(41, vector<int>(2));
+	buff[0][0] = 1;
+	buff[0][1] = 0;
+	buff[1][0] = 0;
+	buff[1][1] = 1;
 	for (int i = 0; i < n; i++)
 	{
 		cin >> num;
 		for (int j = 2; j <= num; j++)
 		{
-			buff[j] = buff[j - 1] + buff[j - 2];
+			buff[j][0] = buff[j - 1][0] + buff[j - 2][0];
+			buff[j][1] = buff[j - 1][1] + buff[j - 2][1];
 		}
-		cout << buff[num].zero_count << " " << buff[num].one_count << "\n";
+		cout << buff[num][0] << " " << buff[num][1] << "\n";
 	}
 }
