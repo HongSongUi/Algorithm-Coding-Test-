f1ef11fd910779bc1123c78a4d3352ae638c358c
thddml <108908383+HongSongUi@users.noreply.github.com>
2024-04-26 23:29:47 +0900

    [Silver I] Title: 회의실 배정, Time: 32 ms, Memory: 2808 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool Compare(pair<int, int >& a, pair<int, int>& b)
+{
+	if (a.second == b.second)
+	{
+		return a.first < b.first;
+	}
+	return a.second < b.second;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	
+	int N = 0;
+	cin >> N;
+
+	int start = 0;
+	int end = 0;
+	vector<pair<int, int>> buff(N);
+	for (int i = 0; i < N; i++)
+	{
+		cin >> start >> end;
+		buff[i] = make_pair(start, end);
+	}
+	sort(buff.begin(), buff.end(), Compare);
+	int count = 1;
+	end = buff[0].second;
+	for (int i = 1; i < buff.size(); i++)
+	{
+		if (buff[i].first >= end)
+		{
+			end = buff[i].second;
+			count++;
+		}
+	}
+	cout << count;
+}
