c8ee378b18aa69a22d228b4415358c1020f09cf2
thddml <108908383+HongSongUi@users.noreply.github.com>
2024-12-01 18:18:11 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+vector<int> result;
+
+void Combination(int start, int depth) 
+{
+    if (depth == m) 
+    {
+        for (int num : result) 
+        {
+            cout << num << " ";
+        }
+        cout << '\n';
+        return;
+    }
+    for (int i = start; i <= n; i++) 
+    { 
+        result.push_back(i);
+        Combination(i + 1, depth + 1); 
+        result.pop_back();
+    }
+}
+
+int main() 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> m;
+    Combination(1, 0);
+}
