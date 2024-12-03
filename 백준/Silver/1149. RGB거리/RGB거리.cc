dc58829d651de4e5d0ae0e84a45ab5306a96e96a
thddml <108908383+HongSongUi@users.noreply.github.com>
2024-12-04 03:21:01 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 6128 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n = 0;
+	cin >> n;
+	vector<vector<int>> arr(n, vector<int>(n, 0));
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			cin >> arr[i][j];
+		}
+	}
+	vector<vector<int>> dp(n, vector<int>(3, 0));
+	for (int i = 0; i < 3; i++)
+	{
+		dp[0][i] = arr[0][i];
+	}
+	for (int i = 1; i < n; i++)
+	{
+		dp[i][0] = arr[i][0] + min(dp[i - 1][1], dp[i - 1][2]);
+		dp[i][1] = arr[i][1] + min(dp[i - 1][0], dp[i - 1][2]);
+		dp[i][2] = arr[i][2] + min(dp[i - 1][0], dp[i - 1][1]);
+	}
+	int answer = *min_element(dp[n - 1].begin(), dp[n - 1].end());
+	cout << answer;
+}
