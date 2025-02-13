f7fe74d42dff8fca1ee94c6cc53071b09a709562
thddml <108908383+HongSongUi@users.noreply.github.com>
2025-02-14 02:24:19 +0900

    Update 방법을 출력하지 않는 숫자 맞추기.cc

--- 백준/Gold/13392. 방법을 출력하지 않는 숫자 맞추기/방법을 출력하지 않는 숫자 맞추기.cc
+++ 백준/Gold/13392. 방법을 출력하지 않는 숫자 맞추기/방법을 출력하지 않는 숫자 맞추기.cc
@@ -1,44 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int MAX_N = 10001;
-const int MAX_ROT = 10;
-int N;
+
+int n = 0;
 string start, target;
 vector<vector<int>> dp;
-int solve(int pos, int rot) {
-    // 기저 사례: 모든 자리를 다 확인했을 때
-    if (pos == N) return 0;
-
-    // 메모이제이션
-    int& ret = dp[pos][rot];
-    if (ret != -1) return ret;
-
-    // 현재 숫자의 실제 값 계산 (누적 회전 적용)
-    int curr = (start[pos] - '0' + rot) % 10;
-    int dest = target[pos] - '0';
-
-    // 왼쪽으로 돌리는 경우
-    int left = (dest - curr + 10) % 10;
-    // 오른쪽으로 돌리는 경우
-    int right = 10 - left;
-
-    // 최소값 계산
-    ret = min(
-        left + solve(pos + 1, (rot + left) % 10),  // 왼쪽으로 돌린 경우
-        right + solve(pos + 1, rot)                // 오른쪽으로 돌린 경우
-    );
-
-    return ret;
+int DP(int pos, int rot)
+{
+    if (pos == n)
+    {
+        return 0;
+    }
+    if (dp[pos][rot] != -1)
+    {
+        return dp[pos][rot];
+    }
+    int curNum = ((start[pos] - '0') + rot) % 10;
+    int targetNum = target[pos] - '0';
+    
+    int left = (targetNum - curNum  + 10) % 10;
+    int right = (10 - left) % 10;
+    
+    dp[pos][rot] = min(left+ DP(pos + 1, (rot + left) % 10), right + DP(pos + 1, rot));
+    return dp[pos][rot];
 }
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    cin >> N;
+    cin >> n;
     cin >> start >> target;
-
-    dp.resize(N, vector<int>(MAX_ROT, -1));
-    cout << solve(0, 0) << '\n';
-
+    dp.resize(n, vector<int>(10, -1));
+    
+    cout << DP(0, 0);
     return 0;
 }
