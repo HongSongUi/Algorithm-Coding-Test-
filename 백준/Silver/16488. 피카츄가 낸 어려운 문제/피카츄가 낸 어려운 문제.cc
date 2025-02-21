3ca2d0026320f02a16c736a7a053620e9dbf7b3d
thddml <108908383+HongSongUi@users.noreply.github.com>
2025-02-21 22:56:53 +0900

    [Silver IV] Title: 피카츄가 낸 어려운 문제, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/16488. 피카츄가 낸 어려운 문제/피카츄가 낸 어려운 문제.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long long n = 0, k = 0;
+    cin >> n >> k;
+    cout << k * n * n;
+    return 0;
+}
