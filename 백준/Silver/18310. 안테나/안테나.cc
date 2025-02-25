f9f62897d6ed2b87420588bc04c438f0e14f4481
thddml <108908383+HongSongUi@users.noreply.github.com>
2025-02-25 23:41:51 +0900

    [Silver III] Title: 안테나, Time: 32 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Silver/18310. 안테나/안테나.cc
+++ 백준/Silver/18310. 안테나/안테나.cc
@@ -13,31 +13,8 @@ int main() {
 		cin >> arr[i];
 	}
 	sort(arr.begin(), arr.end());
-	int midIdx = n / 2;
-	int sum = 0;
-	for (int i = 0; i < n; i++)
-	{
-		sum += abs(arr[midIdx] - arr[i]);
-	}
+	int midIdx = (n-1) / 2;
 	int answer = arr[midIdx];
-	int temp = 0;
-	for (int i = midIdx - 1; i >= 0; i--)
-	{
-		temp = 0;
-		for (int j = 0; j < n; j++)
-		{
-			temp += abs(arr[i] - arr[j]);
-		}
-		if (temp <= sum)
-		{
-			answer = arr[i];
-			sum = temp;
-		}
-		else if (temp > sum)
-		{
-			break;
-		}
-	}
 	cout << answer;
 	return 0;
 }
