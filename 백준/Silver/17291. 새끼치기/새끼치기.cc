0d873e388c19f307aac20422440a63a56476d922
thddml <108908383+HongSongUi@users.noreply.github.com>
2025-03-11 02:19:02 +0900

    [Silver II] Title: 새끼치기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/17291. 새끼치기/새끼치기.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int n = 0;
+	cin >> n;
+	vector<long long> arr(21,0);
+	arr[1] = 1;
+	arr[2] = 2;
+	arr[3] = 4;
+	arr[4] = 7;
+
+	for (int i = 5 ;i <= n; i++)
+	{
+		if (i % 2 == 0)
+		{
+			arr[i] = (arr[i - 1] * 2) - (arr[i - 4] + arr[i - 5]);
+		}
+		else
+		{
+			arr[i] = arr[i - 1] * 2;
+		}
+	}
+	cout << arr[n];
+
+	return 0;
+}
