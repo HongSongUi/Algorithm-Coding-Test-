d249e5e00415641b89e343f39d1da7b5c85c5f64
thddml <108908383+HongSongUi@users.noreply.github.com>
2025-05-21 01:10:08 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 28 ms, Memory: 2296 KB -BaekjoonHub

---
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	int t = 0;
+	cin >> t;
+	int n, m = 0;
+	while (t--)
+	{
+		cin >> n >> m;
+		vector<int> arr1(n);
+		vector<int> arr2(m);
+		for (int i = 0; i < n; i++)
+		{
+			cin >> arr1[i];
+		}
+		for (int i = 0; i < m; i++)
+		{
+			cin >> arr2[i];
+		}
+		sort(arr1.begin(), arr1.end());
+		sort(arr2.begin(), arr2.end());
+		int answer = 0;
+		for (int i = 0; i < m; i++)
+		{
+			int target_num = arr2[i];
+			int idx = upper_bound(arr1.begin(), arr1.end(), target_num) - arr1.begin();
+			answer += (n - idx);
+		}
+		cout << answer << '\n';
+	}
+	return 0;
+}
