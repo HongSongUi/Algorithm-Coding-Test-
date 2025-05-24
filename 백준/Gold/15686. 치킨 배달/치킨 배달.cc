3fbf405880c9c6f5b97fcc5ac43dae918c14ef19
thddml <108908383+HongSongUi@users.noreply.github.com>
2025-05-25 00:48:37 +0900

    [Gold V] Title: 치킨 배달, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -1,76 +1,78 @@
+/*
+치킨집과 집 사이의 거리를 계산하는 문제이기 때문에 주어진 전체 graph를 전부 저장하지 않고 
+집과 치킨집의 좌표만 저장
+치킨집이 전체 K개라고 했을 때 그 중 주어진 m개만 선택 -> 조합 사용
+
+
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
-int n, m;
-vector<pair<int, int>> town;
+int n = 0, m = 0;
+vector<pair<int, int>> house;
 vector<pair<int, int>> chicken;
-vector<vector<int>> dist;
-int sumMin = INT_MAX;
-void Calc(const vector<int>& temp)
+vector<pair<int, int>> tmp;
+
+int answer = INT_MAX;
+
+void FindDist(int count,int idx)
 {
-    int d = 0;
-    for (int i = 0; i < town.size(); i++)
-    {
-        int t = INT_MAX;
-        for (int j = 0; j < temp.size(); j++)
-        {
-            int index = temp[j];
-            t = min(t, dist[i][index]);
-        }
-        d += t;
-    }
-    sumMin = min(sumMin, d);
+	if (count == m)
+	{
+		int total = 0;
+		for (int i = 0; i < house.size(); i++) //각 집 별로 가장 가까운 치킨 집의 거리
+		{
+			int min_dist = INT_MAX;
+			int x = house[i].first;
+			int y = house[i].second;
+			for (int j = 0; j < tmp.size(); j++)
+			{
+				int tx = tmp[j].first;
+				int ty = tmp[j].second;
+				int dist = abs(tx - x) + abs(ty - y);
+				if (dist < min_dist)
+				{
+					min_dist = dist;
+				}
+			}
+			total += min_dist;
+		}
+		answer = min(total, answer);
+		return;
+	}
+	for (int i = idx; i < chicken.size(); i++)
+	{
+
+		tmp.emplace_back(make_pair(chicken[i].first, chicken[i].second)); // 저장
+		FindDist(count + 1, i+1);
+		tmp.pop_back(); // 상태 복구
+		
+	}
 }
-void BackTraking(int start, vector<int>& temp)
+
+int main()
 {
-    if (temp.size() == m)
-    {
-        Calc(temp);
-        return;
-    }
-    for (int i = start; i < chicken.size(); i++)
-    {
-        temp.push_back(i);
-        BackTraking(i + 1, temp);
-        temp.pop_back();
-    }
-}
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-
-   
-    cin >> n >> m;
-
-    int tCount = 0, mCount = 0;
-    int num = 0;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            cin >> num;
-            if (num == 1)
-            {
-                town.emplace_back(make_pair(j, i));
-            }
-            else if (num == 2)
-            {
-                chicken.emplace_back(make_pair(j, i));  
-            }
-        }
-    }
-    dist.resize(town.size(), vector<int>(chicken.size()));
-    for (int i = 0; i < town.size(); i++)
-    {
-        for (int j = 0; j < chicken.size(); j++)
-        {
-            dist[i][j] = abs(town[i].first - chicken[j].first) + abs(town[i].second - chicken[j].second);
-        }
-    }
-    
-    vector<int> temp;
-    BackTraking(0, temp);
-   
-    cout << sumMin;
-    return 0;
+	cin >> n >> m;
+	int num = 0;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cin >> num;
+			if (num == 1)
+			{
+				house.emplace_back(make_pair(j, i));//집 좌표
+			}
+			else if (num == 2)
+			{
+				chicken.emplace_back(make_pair(j, i)); // 치킨집 좌표
+			}
+		}
+	}
+	FindDist(0,0);
+	cout << answer;
+	return 0;
 }
