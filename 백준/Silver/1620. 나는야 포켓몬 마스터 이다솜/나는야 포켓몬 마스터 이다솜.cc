276d56d47efebd395345aafd75cbb00cbb282f41
thddml <108908383+HongSongUi@users.noreply.github.com>
2025-06-10 18:43:57 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 212 ms, Memory: 23944 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -1,39 +1,38 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int main()
-{
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	
-	int n = 0;
-	int k = 0;
-	cin >> n >> k;
-	string name = "";
-	map<string, int> name_buff;
-	map<int, string> id_buff;
-	int find_id = 0;
-	string find_name = "";
-	for (int i = 0; i < n; i++)
-	{
-		cin >> name;
-		name_buff.insert(make_pair(name, i + 1));
-		id_buff.insert(make_pair(i + 1, name));
-	}
-	for (int i = 0; i < k; i++)
-	{
-		cin >> name;
-		if (name[0] >= 'A' && name[0] <= 'Z')
-		{
-			find_id = name_buff[name];
-			cout << find_id << "\n";
-		}
-		else
-		{
-			int temp = stoi(name);
-			find_name = id_buff[temp];
-			cout << find_name << "\n";
-		}
-	}
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n = 0, m;
+
+    cin >> n >> m;
+    map<string, int> name;
+    map<int, string> idx;
+    string s;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> s;
+        name.insert(make_pair(s, i+1));
+        idx.insert(make_pair(i+1, s));
+    }
+    for (int i = 0; i < m; i++)
+    {
+        cin >> s;
+        if (s[0] >= '0' && s[0] <= '9')
+        {
+            int num = stoi(s);
+            cout << idx[num] << '\n';
+        }
+        else
+        {
+            cout << name[s] << '\n';
+        }
+    }
+
+    return 0;
 }
