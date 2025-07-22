191e3a515c85db43dc1e48165d1b3199a305d316
thddml <108908383+HongSongUi@users.noreply.github.com>
2025-07-23 02:19:33 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 36 ms, Memory: 3356 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -1,29 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> p;
-
+vector<int>p;
 int find(int x)
 {
-    if (p[x] < 0) return x;
+    if (p[x] ==x ) return x;
     return p[x] = find(p[x]);
 }
 
-bool Uni(int u, int v)
+bool isUnion(int u, int v)
 {
     u = find(u);
     v = find(v);
-    if (u == v) return false;
-    if (p[u] == p[v]) p[u]--;
-    
-    if (p[u] < p[v])
-    {
-        p[v] = u;
-    }
-    else
-    {
-        p[u] = v;
-    }
+    if (u == v)return false;
+    p[u] = v;
     return true;
 }
 
@@ -31,29 +21,33 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-
+    
     int n = 0, m = 0;
     cin >> n >> m;
-    vector<tuple<int, int, int>> edges(m);
-    p.resize(n + 1, -1);
-    int n1 = 0, n2 = 0, cost = 0;
+    p.resize(n + 1);
+    for (int i = 1; i <= n; i++)
+    {
+        p[i] = i;
+    }
+    vector<tuple<int, int, int>>edges(m);
+    int node1, node2, cost;
     for (int i = 0; i < m; i++)
     {
-        cin >> n1 >> n2 >> cost;
-        edges[i] = { cost,n1,n2 };
+        cin >> node1 >> node2 >> cost;
+        edges[i] = { cost,node1,node2 };
     }
     sort(edges.begin(), edges.end());
-    int cnt = 0;
     int answer = 0;
+    int cnt = 0;
     for (int i = 0; i < m; i++)
     {
-       n1 = get<1>(edges[i]);
-       n2 = get<2>(edges[i]);
-       cost = get<0>(edges[i]);
-       if (!Uni(n1, n2)) continue;
-       answer += cost;
-       cnt++;
-       if (cnt == n - 1) break;
+        int u = get<1>(edges[i]);
+        int v = get<2>(edges[i]);
+        int c = get<0>(edges[i]);
+        if (!isUnion(u, v)) continue;
+        answer+= c;
+        cnt++;
+        if (cnt == n - 1) break;
     }
     cout << answer;
     return 0;
