922c13c673a2f85d14389bb35809eefdaaace300
thddml <108908383+HongSongUi@users.noreply.github.com>
2026-01-23 00:44:54 +0900

    [Gold IV] Title: 연구소, Time: 112 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.cc
+++ 백준/Gold/14502. 연구소/연구소.cc
@@ -1,101 +1,105 @@
 #include <bits/stdc++.h>
+#include <unordered_map>
+
 using namespace std;
+int n = 0;
+int m = 0;
 vector<vector<int>> graph;
+vector<pair<int, int>> sPoints;
 
-int n, m;
 vector<int> dx = { -1,0,1,0 };
-vector<int> dy = { 0,1,0,-1, };
+vector<int> dy = { 0,-1,0,1 };
+int mSize = 0;
 
-vector<pair<int,int>> virus;
-int maxCount = -1;
-bool CheckPoint(int x, int y)
+bool IsValid(int x, int y)
 {
-    if ((x >= 0 && x < m) && (y >= 0 && y < n))
-    {
-        return true;
-    }
-    return false;
+	return (x >= 0 && x < m) && (y >= 0 && y < n);
 }
+
 void BFS()
 {
-    vector<vector<int>> temp = graph;
-    int zeroCount = 0;
-    queue<pair<int,int>>q;
-    for (int i = 0; i < virus.size(); i++)
-    {
-        q.push(virus[i]);
-    }
-    while (!q.empty())
-    {
-        int cur_x = q.front().first;
-        int cur_y = q.front().second;
-        q.pop();
-        for (int i = 0; i < 4; i++)
-        {
-            int next_x = cur_x + dx[i];
-            int next_y = cur_y + dy[i];
-            if (CheckPoint(next_x, next_y) && temp[next_y][next_x] == 0)
-            {
-                temp[next_y][next_x] = 2;
-                q.push(make_pair( next_x,next_y ));
-            }
-        }
-    }
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            if (temp[i][j] == 0)
-            {
-                zeroCount++;
-            }
-        }
-    }
-    maxCount = max(maxCount, zeroCount);
+	vector<vector<int>> tmp = graph;
+	vector<vector<bool>> visited(n, vector<bool>(m, false));
+	queue<pair<int, int>> q;
+	for (int i = 0; i < sPoints.size(); i++)
+	{
+		q.push(make_pair(sPoints[i].first, sPoints[i].second));
+		visited[sPoints[i].second][sPoints[i].first] = true;
+	}
+	while (!q.empty())
+	{
+		int cur_x = q.front().first;
+		int cur_y = q.front().second;
+
+		q.pop();
+		for (int i = 0; i < 4; i++)
+		{
+			int nx = cur_x + dx[i];
+			int ny = cur_y + dy[i];
+			if (IsValid(nx, ny) && !visited[ny][nx] && tmp[ny][nx] != 1 && tmp[ny][nx] != 2)
+			{
+				q.push(make_pair(nx, ny));
+				visited[ny][nx] = true;
+				tmp[ny][nx] = 2;
+			}
+		}
+	}
+	int s = 0;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			if (tmp[i][j] == 0) s++;
+		}
+	}
+	mSize = max(mSize, s);
+	return;
 }
-void Wall(int count)
+
+void MakeWall(int cnt, int sx)
 {
-    if (count == 3)
-    {
-        BFS();
-        return;
-    }
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            if (graph[i][j] == 0)
-            {
-                graph[i][j] = 1;
-                Wall(count + 1);
-                graph[i][j] = 0;
-            }
-        }
-    }
-
+	if (cnt == 3)
+	{
+		BFS();
+		return;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = sx; j < m; j++)
+		{
+			if (graph[i][j] == 0)
+			{
+				graph[i][j] = 1;
+				MakeWall(cnt + 1, j);
+				graph[i][j] = 0;
+			}
+		}
+	}
 }
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
 
-    cin >> n >> m;
-    graph.resize(n, vector<int>(m, 0));
-    
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            cin >> graph[i][j];
-            if (graph[i][j] == 2)
-            {
-                virus.emplace_back(make_pair( j,i ));
-            }
-        }
-    }
-    int answer = 0;
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    Wall(0);
-    cout << maxCount;
-    return 0;
+
+	cin >> n >> m;
+
+	graph.resize(n, vector<int>(m));
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cin >> graph[i][j];
+			if (graph[i][j] == 2)
+			{
+				sPoints.emplace_back(make_pair(j, i));
+			}
+		}
+	}
+	MakeWall(0,0);
+	cout << mSize;
+	return 0;
 }
