ede0bdc41c5ae07bc2e0de2bb451d1826c2acc48
thddml <108908383+HongSongUi@users.noreply.github.com>
2026-01-22 23:55:44 +0900

    [Silver V] Title: 역원소 정렬, Time: 0 ms, Memory: 2224 KB -BaekjoonHub

--- 백준/Silver/5648. 역원소 정렬/역원소 정렬.cc
+++ 백준/Silver/5648. 역원소 정렬/역원소 정렬.cc
@@ -1,26 +1,32 @@
 #include <bits/stdc++.h>
+#include <unordered_map>
+
 using namespace std;
+
 
 
 int main()
 {
 	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
 	int n = 0;
 	cin >> n;
-	vector<long long> arr(n);
-	string s = "";
+	vector <long long> vec(n);
+
+	string tmp = "";
 	for (int i = 0; i < n; i++)
 	{
-		cin >> s;
-		reverse(s.begin(), s.end());
-		arr[i] = (stoll(s));
+		cin >> tmp;
+		reverse(tmp.begin(), tmp.end());
+		long long ll = stoll(tmp);
+		vec[i] = ll;
 	}
-	sort(arr.begin(), arr.end());
+	sort(vec.begin(), vec.end());
 	for (int i = 0; i < n; i++)
 	{
-		cout << arr[i] << '\n';
+		cout << vec[i] << '\n';
 	}
 	return 0;
 }
