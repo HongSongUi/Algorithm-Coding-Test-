de18fb543b1e0ce5452105efb02908ef922d9d3e
thddml <108908383+HongSongUi@users.noreply.github.com>
2026-01-24 01:56:56 +0900

    [Silver II] Title: 나무 자르기, Time: 148 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.cc
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.cc
@@ -1,43 +1,46 @@
 #include <bits/stdc++.h>
-using namespace std;
+#include <unordered_map>
 
+#define INF 20000001
+using namespace std;
 
 
 int main()
 {
 	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-	int n = 0, m = 0;
+	int n = 0;
+	int m = 0;
 	cin >> n >> m;
-	vector<int> trees(n, 0);
+	vector<int> vec(n);
 	for (int i = 0; i < n; i++)
 	{
-		cin >> trees[i];
+		cin >> vec[i];
 	}
-	sort(trees.begin(), trees.end());
-	int left = 0;
-	int right = trees[n - 1];
+	long long low = 1;
+	long long high = *max_element(vec.begin(), vec.end());
 	long long answer = 0;
-	while (left <= right)
+	while (low <= high)
 	{
-		long long mid = (left + right) / 2;
-		long long total = 0;
+		long long mid = (low + high) / 2;
+		long long cnt = 0;
 		for (int i = 0; i < n; i++)
 		{
-			if (trees[i] >= mid) 
+			if ((vec[i] - mid) > 0)
 			{
-				total += trees[i] - mid;
+				cnt += (vec[i] - mid);
 			}
 		}
-		if (total >= m)
+		if (cnt >= m)
 		{
 			answer = mid;
-			left = mid + 1;
+			low = mid + 1;
 		}
 		else
 		{
-			right = mid - 1;
+			high = mid - 1;
 		}
 	}
 	cout << answer;
