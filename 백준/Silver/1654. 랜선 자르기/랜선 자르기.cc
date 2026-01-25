6c6ec70015e5726f5eb3298d6e6fb096ca2ac968
thddml <108908383+HongSongUi@users.noreply.github.com>
2026-01-26 02:39:27 +0900

    [Silver II] Title: 랜선 자르기, Time: 4 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -1,56 +1,49 @@
 #include <bits/stdc++.h>
+#include <unordered_map>
+
 using namespace std;
 
-
-void Search(long long start, long long end, long long& result, int n, vector<long long>& buff)
-{
-	if (start > end)
-	{
-		return;
-	}
-	long long mid = (start + end) / 2;
-	int count = 0;
-	for (int i = 0; i < buff.size(); i++)
-	{
-		count = count + (buff[i] / mid);
-	}
-	if (count >= n)
-	{
-		result = mid;
-		start = mid + 1;
-		Search(start, end, result, n, buff);
-	}
-	else
-	{
-		end = mid - 1;
-		Search(start, end, result, n, buff);
-	}
-}
 
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	
-	int k = 0;
+
 	int n = 0;
-	cin >> k;
-	cin >> n;
-	vector<long long>buff(k);
-	long long start = 1;
-	long long end = 0;
-	long long mid = 0;
-	long long count = 0;
-	long long result = 0;
-	for (int i = 0; i < k; i++)
+	int c = 0;
+	cin >> n >> c;
+	vector<int> vec(n);
+	for (int i = 0; i < n; i++)
 	{
-		cin >> buff[i];
-		if (buff[i] >= end)
-		{
-			end = buff[i];
-		}
+		cin >> vec[i];
 	}
-	Search(start, end, result, n, buff);
-    cout << result;
+	sort(vec.begin(), vec.end());
+
+	long long low = 1;
+	long long high = vec[n-1];
+
+	long long answer = 0;
+	while (low <= high)
+	{
+		long long mid = (low + high) / 2;
+		int cnt = 0;
+		for (int i = 0; i < n; i++)
+		{
+			cnt += (vec[i] / mid);
+		}
+
+		if (cnt >= c)
+		{
+			answer = max(mid, answer);
+			low = mid + 1;
+		}
+		else
+		{
+			high = mid - 1;
+		}
+
+	}
+	cout << answer;
+	return 0;
 }
