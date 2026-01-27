838664f6cc66e7eb127a36abfb89cb557d1f873d
thddml <108908383+HongSongUi@users.noreply.github.com>
2026-01-27 23:18:36 +0900

    [Gold IV] Title: 인구 이동, Time: 780 ms, Memory: 2164 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.cc
+++ 백준/Gold/16234. 인구 이동/인구 이동.cc
@@ -1,106 +1,96 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 int n = 0;
 int l = 0, r = 0;
-vector<int> dx = { -1,0,1,0 };
-vector<int> dy = { 0,-1,0,1 };
-vector<vector<int>> arr;
+vector<vector<int>> vec;
 
-bool IsVaild(int x, int y)
+vector<pair<int, int>> dir = { {-1,0},{0,-1},{1,0},{0,1} };
+
+bool IsValid(int x, int y)
 {
 	return (x >= 0 && x < n) && (y >= 0 && y < n);
 }
-bool IsUnion(int val)
+bool Diff(int cx,int cy, int nx,int ny)
 {
-	return val >= l && val <= r;
+	int d = abs(vec[cy][cx] - vec[ny][nx]);
+	return (d >= l && d <= r);
 }
-bool bfs(int start_x, int start_y , vector<vector<bool>>& visited)
+void BFS(int x, int y, vector<vector<bool>>& visited, vector<vector<int>>& tmp)
 {
+	int num = 0;
+	visited[y][x] = true;
 	queue<pair<int, int>> q;
-	int total = 0;
-	int count = 0;
-	visited[start_y][start_x] = true;
-	q.push(make_pair(start_y, start_x));
-	queue<pair<int, int>> uni;
-	total = 0;
-	count = 0;
+	q.push(make_pair(x, y));
+	vector<pair<int, int>> s;
+
 	while (!q.empty())
 	{
-		int cur_x = q.front().second;
-		int cur_y = q.front().first;
-		int cur_num = arr[cur_y][cur_x];
-		uni.push(make_pair(cur_x, cur_y));
-		total += cur_num;
-		count++;
+		int cur_x = q.front().first;
+		int cur_y = q.front().second;
+		s.emplace_back(make_pair(cur_x, cur_y));
+
+		num += vec[cur_y][cur_x];
 		q.pop();
+		
 		for (int i = 0; i < 4; i++)
 		{
-			int next_x = cur_x + dx[i];
-			int next_y = cur_y + dy[i];
-			if (IsVaild(next_x, next_y) && !visited[next_y][next_x])
+			int nx = cur_x + dir[i].first;
+			int ny = cur_y + dir[i].second;
+			if (IsValid(nx, ny) && !visited[ny][nx] && Diff(cur_x,cur_y,nx,ny))
 			{
-				int next_num = arr[next_y][next_x];
-				if (IsUnion(abs(next_num - cur_num)))
-				{
-					q.push(make_pair(next_y, next_x));
-					visited[next_y][next_x] = true;
-				}
+				visited[ny][nx] = true;
+				q.push(make_pair(nx, ny));
 			}
 		}
 	}
-	if (uni.size() == 1) return false;
-
-	while (!uni.empty())
+	int val = num / s.size();
+	for (int i = 0; i < s.size(); i++)
 	{
-		int cur_x = uni.front().first;
-		int cur_y = uni.front().second;
-		arr[cur_y][cur_x] = total / count;
-		uni.pop();
+		int x = s[i].first;
+		int y = s[i].second;
+		tmp[y][x] = val;
 	}
-	return true;
 }
+
 
 int main()
 {
 	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
 
 	cin >> n >> l >> r;
-	arr.resize(n, vector<int>(n));
+	vec.resize(n, vector<int>(n));
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < n; j++)
 		{
-			cin >> arr[i][j];
+			cin >> vec[i][j];
 		}
 	}
 	int days = 0;
-	bool isFind = false;
 	while (true)
 	{
 		vector<vector<bool>> visited(n, vector<bool>(n, false));
-		bool moved = false;
-
-		for (int i = 0; i < n; i++) 
+		vector<vector<int>> tmp(n, vector<int>(n));
+		tmp = vec;
+		for (int i = 0; i < n; i++)
 		{
 			for (int j = 0; j < n; j++)
 			{
-				if (!visited[i][j]) 
+				if (!visited[i][j])
 				{
-					if (bfs(j, i, visited))
-					{
-						moved = true;
-					}
+					BFS(j, i, visited, tmp);
 				}
 			}
 		}
+		if (tmp == vec) break;
 
-		if (!moved) break;
+		vec = tmp;
 		days++;
-	
-		
 	}
 	cout << days;
 	return 0;
