39a1584bc49d41c4f4b8a0f46d97a7459401f1a9
thddml <108908383+HongSongUi@users.noreply.github.com>
2026-01-27 19:06:19 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 8 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
@@ -1,64 +1,89 @@
 #include <bits/stdc++.h>
+
 using namespace std;
-int n = 0;
-vector<int> arr;
-vector<int> oper(4);
-vector<int> tmp;
-vector<bool> visited;
-int max_answer = INT_MIN;
-int min_answer = INT_MAX;
-void Func(int idx , int current)
+
+vector<int> numbers;
+vector<int> opers;
+vector<string> choose;
+map<int, string> tmp;
+int max_val = INT_MIN;
+int min_val = INT_MAX;
+void Calc()
 {
-	if (idx == n-1)
+	int cur_num = numbers[0];
+	int next_num = 0;
+	string oper = "";
+	for (int i = 1; i < numbers.size(); i++)
 	{
-		max_answer = max(max_answer, current);
-		min_answer = min(min_answer, current);
+		next_num = numbers[i];
+		oper = choose[i-1];
+		if (oper == "+")
+		{
+			cur_num += next_num;
+		}
+		else if (oper == "-")
+		{
+			cur_num -= next_num;
+		}
+		else if (oper == "*")
+		{
+			cur_num *= next_num;
+		}
+		else if (oper == "/")
+		{
+			cur_num /= next_num;
+		}
+	}
+	min_val = min(min_val, cur_num);
+	max_val = max(max_val, cur_num);
+	return;
+}
+
+void BackTracking(int cnt, int limit)
+{
+	if (cnt == limit)
+	{
+		Calc();
 		return;
 	}
-	for (int i = 0; i < 4; i++)
+	for (int i = 0; i < opers.size(); i++)
 	{
-		if (oper[i] >0)
+		if (opers[i] > 0)
 		{
-			oper[i]--;
-			int next = current;
-			if (i == 0)
-			{
-				next += arr[idx + 1];
-			}
-			else if (i == 1)
-			{
-				next -= arr[idx + 1];
-			}
-			else if (i == 2)
-			{
-				next *= arr[idx + 1];
-			}
-			else if (i == 3)
-			{
-				next /= arr[idx + 1];
-			}
-			Func(idx + 1,next);
-			oper[i]++;
+			choose.emplace_back(tmp[i]);
+			opers[i]--;
+			BackTracking(cnt + 1, limit);
+			opers[i]++;
+			choose.pop_back();
 		}
 	}
 }
+
 int main()
 {
 	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
+	int n = 0;
 	cin >> n;
-	arr.resize(n);
-	visited.resize(4);
+	numbers.resize(n);
+	opers.resize(4);
+	int limit = 0;
 	for (int i = 0; i < n; i++)
 	{
-		cin >> arr[i];
+		cin >> numbers[i];
 	}
 	for (int i = 0; i < 4; i++)
 	{
-		cin >> oper[i];
+		cin >> opers[i];
+		limit += opers[i];
 	}
-	Func(0, arr[0]);
-	cout << max_answer << '\n' << min_answer;
+	tmp[0] = "+";
+	tmp[1] = "-";
+	tmp[2] = "*";
+	tmp[3] = "/";
+	BackTracking(0, limit);
+	cout << max_val << '\n' << min_val;
 	return 0;
 }
