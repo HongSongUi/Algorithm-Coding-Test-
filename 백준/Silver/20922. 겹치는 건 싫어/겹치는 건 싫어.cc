ddda140dce6a8556c842097c350386a9d47cca53
thddml <108908383+HongSongUi@users.noreply.github.com>
2026-02-01 03:07:27 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 112 ms, Memory: 7168 KB -BaekjoonHub

--- 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.cc
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.cc
@@ -1,37 +1,42 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    int n = 0, k = 0;
-    cin >> n >> k;
-    vector<int> arr(n);
-
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
-    int answer = 0;
-    int count = 0;
-    int right =0, left = 0;
-    vector<int> tmp(100001, 0);
-    while (right < n)
-    {
-        if (tmp[arr[right]]< k)
-        {
-            tmp[arr[right]] ++;
-            right++;
-        }
-        else
-        {
-            tmp[arr[left]]--;
-            left++;
-        }
-       
-        answer = max(answer, right - left);
-    }
-    cout << answer;
+	int n = 0, k = 0;
+	cin >> n >> k;
+	vector<int> vec(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> vec[i];
+	}
+	map<int, int> cnt;
+	int left = 0;
+	int right = 0;
+	int cur_len = 0;
+	int max_len = 0;
+	while (true)
+	{
+		if (right == vec.size() )break;
+		if (cnt[vec[right]] <= k)
+		{
+			cnt[vec[right]]++;
+			while (cnt[vec[right]] > k)
+			{
+				cnt[vec[left]]--;
+				left++;
+			}
+			right++;
+		}
+		cur_len = right - left;
+		max_len = max(max_len, cur_len);
+	}
+	cout << max_len;
+	return 0;
 }
