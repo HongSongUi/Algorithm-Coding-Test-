50d8a57820fa7bbd1aa65d044a43476b39904527
thddml <108908383+HongSongUi@users.noreply.github.com>
2026-01-31 22:21:42 +0900

    [Gold V] Title: 가장 긴 짝수 연속한 부분 수열 (large), Time: 68 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Gold/22862. 가장 긴 짝수 연속한 부분 수열 （large）/가장 긴 짝수 연속한 부분 수열 （large）.cc
+++ 백준/Gold/22862. 가장 긴 짝수 연속한 부분 수열 （large）/가장 긴 짝수 연속한 부분 수열 （large）.cc
@@ -1,35 +1,48 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
 
-    int n = 0, k = 0;
-    cin >> n >> k;
-    vector<int> arr(n);
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
-    
-    int answer = 0;
-    int left = 0;
-    int right = 0;
-    int odd_count = 0;
-    int tmp = 0;
-    while (left < n)
-    {
-        while (right < n && odd_count <= k)
-        {
-            if (arr[right] % 2 != 0) odd_count++;
-            right++;
-        }
-        tmp = (right - left) - odd_count;
-        answer = max(answer, tmp);
-        if (arr[left] % 2 != 0) odd_count--;
-        left++;
-    }
-    cout << answer;
-    return 0;
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n = 0, k = 0;
+	cin >> n >> k;
+	vector<int>vec(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> vec[i];
+	}
+	int right = 0;
+	int left = 0;
+	int max_len = 0;
+	int cur_len = 0;
+	int cnt = 0;
+	while (true)
+	{
+		if (cnt <= k)
+		{
+			if (right == vec.size()) break;
+
+			if (vec[right] % 2 != 0)
+			{
+				cnt++;
+			}
+			right++;
+		}
+		
+			while (cnt > k)
+			{
+				if (vec[left] % 2 != 0) cnt--;
+				left++;
+			}
+		
+		cur_len = (right - left - cnt);
+		max_len = max(max_len, cur_len);
+	}
+	cout << max_len;
+	return 0;
 }
