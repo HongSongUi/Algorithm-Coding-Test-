ce044db686f6d5fe2962869b80449c1844d1c880
thddml <108908383+HongSongUi@users.noreply.github.com>
2026-02-08 01:02:21 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 176 ms, Memory: 25528 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -3,36 +3,38 @@
 using namespace std;
 
 
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    
-    int n = 0, m;
+	int n = 0, m = 0;
+	cin >> n >> m;
 
-    cin >> n >> m;
-    map<string, int> name;
-    map<int, string> idx;
-    string s;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> s;
-        name.insert(make_pair(s, i+1));
-        idx.insert(make_pair(i+1, s));
-    }
-    for (int i = 0; i < m; i++)
-    {
-        cin >> s;
-        if (s[0] >= '0' && s[0] <= '9')
-        {
-            int num = stoi(s);
-            cout << idx[num] << '\n';
-        }
-        else
-        {
-            cout << name[s] << '\n';
-        }
-    }
+	map<string, int> name_key;
+	map<int, string> number_key;
 
-    return 0;
+	string name;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> name;
+		name_key[name] = i + 1;
+		number_key[i + 1] = name;
+	}
+	string s;
+	for (int i = 0; i < m; i++)
+	{
+		cin >> s;
+		if (s[0] >= '0' && s[0] <= '9')
+		{
+			int k = stoi(s);
+			cout << number_key[k] << '\n';
+		}
+		else
+		{
+			cout << name_key[s] << '\n';
+		}
+	}
+	return 0;
 }
