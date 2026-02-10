87af2cf3a4c54137a64dc6a0f22df53445cfb625
thddml <108908383+HongSongUi@users.noreply.github.com>
2026-02-11 01:59:50 +0900

    [Silver III] Title: N번째 큰 수, Time: 232 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.cc
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.cc
@@ -1,27 +1,31 @@
 #include <bits/stdc++.h>
+#include <unordered_map>
 using namespace std;
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    
-    int n = 0;
-    cin >> n;
-    
-    priority_queue<int> pq;
-    int num = 0;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            cin >> num;
-            pq.push(-num);
-            if (pq.size() > n)
-            {
-                pq.pop();
-            }
-        }
-    }
-    cout << -pq.top();
-    return 0;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n = 0;
+	cin >> n;
+	priority_queue<int,vector<int>, greater<int>> pq;
+
+	int tmp = 0;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cin >> tmp;
+			pq.push(tmp);
+			if (pq.size() > n)
+			{
+				pq.pop();
+			}
+		}
+	}
+	cout << pq.top();
+	return 0;
 }
