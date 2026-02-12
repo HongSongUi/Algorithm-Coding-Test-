55fee505010f2333b8e8e0b099aa7a9660e69a77
thddml <108908383+HongSongUi@users.noreply.github.com>
2026-02-13 01:53:00 +0900

    [Gold III] Title: 마법사 상어와 토네이도, Time: 36 ms, Memory: 3088 KB -BaekjoonHub

--- 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.cc
+++ 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.cc
@@ -1,113 +1,96 @@
 #include <bits/stdc++.h>
+
 using namespace std;
-vector<vector<int>> left_dir = { {0,0,2,0,0},{0,10,7,1,0},{5,-1,-1,-1,0},{0,10,7,1,0},{0,0,2,0,0} };
-vector<vector<int>> right_dir = { {0,0,2,0,0},{0,1,7,10,0},{0,-1,-1,-1,5},{0,1,7,10,0},{0,0,2,0,0} };
-vector<vector<int>> top_dir = { {0,0,5,0,0},{0,10,-1,10,0},{2,7,-1,7,2},{0,1,-1,1,0},{0,0,0,0,0} };
-vector<vector<int>> bottom_dir= { {0,0,0,0,0},{0,1,-1,1,0},{2,7,-1,7,2},{0,10,-1,10,0} ,{0,0,5,0,0} };
-vector<pair<int, int>> direction = { {0,-1},{1,0},{0,1},{-1,0} };//y,x
-vector<vector<long long>> arr;
+
+vector<pair<int, int>> dir = { {-1,0}, {0,1},{1,0},{0,-1} };
+
+vector<vector<int>> lf = { {0,0,2,0,0}, {0,10,7,1,0},{5,0,0,0,0} , {0,10,7,1,0},{0,0,2,0,0} };
+vector<vector<int>> btm = { {0,0,0,0,0},{0,1,0,1,0}, {2,7,0,7,2},{0,10,0,10,0}, {0,0,5,0,0} };
+vector<vector<int>> rt = { {0,0,2,0,0} , {0,1,7,10,0}, {0,0,0,0,5}, {0,1,7,10,0}, {0,0,2,0,0} };
+vector<vector<int>> up_d = { {0,0,5,0,0},{0,10,0,10,0},{2,7,0,7,2},{0,1,0,1,0} , {0,0,0,0,0} };
 int n = 0;
+vector<vector<int>> vec;
 
 bool IsValid(int x, int y)
 {
-    return (x >= 0 && x < n) && (y >= 0 && y < n);
+	return (x >= 0 && x < n) && (y >= 0 && y < n);
 }
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
 
-    cin >> n;
-    arr.resize(n, vector<long long>(n));
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            cin >> arr[i][j];
-        }
-    }
-    int cur_x = n / 2;
-    int cur_y = n / 2;
-    int idx = 0;
-    pair<int, int> dir;
-    int limit = 1;
-    int count = 0;
-    vector<vector<int>> tmp;
-    long long answer = 0;
-    while (true)
-    {
-       
-        dir = direction[idx];
-        cur_x += dir.second;
-        cur_y += dir.first;
-        if (arr[cur_y][cur_x] != 0)
-        {
-            long long dust = arr[cur_y][cur_x];
-            long long remain_dust = dust;
-            pair<int, int> alpha;
-            int nx = 0;
-            int ny = 0;
-            if (idx == 0)
-            {
-                tmp = left_dir;
-                alpha = make_pair(cur_y, cur_x - 1);
-            }
-            else if (idx == 1)
-            {
-                tmp = bottom_dir;
-                alpha = make_pair(cur_y+1, cur_x);
-            }
-            else if (idx == 2)
-            {
-                tmp = right_dir;
-                alpha = make_pair(cur_y , cur_x+1);
-            }
-            else
-            {
-                tmp = top_dir;
-                alpha = make_pair(cur_y-1, cur_x);
-            }
-            for (int i = 0; i < 5; i++)
-            {
-                for (int j = 0; j < 5; j++)
-                {
-                    if (tmp[i][j] == 0 || tmp[i][j] == -1) continue;
-                    int tx = j - 2;
-                    int ty = i - 2;
-                    ny = cur_y + ty;
-                    nx = cur_x + tx;
-                    long long move_dust = dust * tmp[i][j] / 100;
-                    if (IsValid(nx, ny))
-                    {
-                        arr[ny][nx] += move_dust;
-                        remain_dust -= move_dust;
-                    }
-                    else
-                    {
-                        answer += move_dust;
-                        remain_dust -= move_dust;
-                    }
-                }
-            }
-            arr[cur_y][cur_x] = 0;
-            if (IsValid(alpha.second, alpha.first))
-            {
-                arr[alpha.first][alpha.second] += remain_dust;
-            }
-            else
-            {
-                answer += remain_dust;
-            }
-        }
-        count++;
-        if (count == limit)
-        {
-            count = 0;
-            idx++;
-            idx %= 4;
-            if (idx % 2 == 0) limit++;
-        }
-        if (cur_y == 0 && cur_x == 0) break;   
-    }
-    cout << answer;
-    return 0;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	cin >> n;
+	vec.resize(n, vector<int>(n));
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cin >> vec[i][j];
+		}
+	}
+	int cur_x = n / 2;
+	int cur_y = n / 2;
+	int idx = 0;
+	int limit = 1;
+	int answer = 0;
+	while (true)
+	{
+		pair<int, int> cur_dir = dir[idx];
+		vector<vector<int>> tmp;
+		if (idx == 0) tmp = lf;
+		else if (idx == 1) tmp = btm;
+		else if (idx == 2) tmp = rt;
+		else tmp = up_d;
+		for (int p = 0; p < limit; p++)
+		{
+			int nx = cur_x + cur_dir.first;
+			int ny = cur_y + cur_dir.second;
+			int amount = vec[ny][nx];
+			int dust = 0;
+			for (int i = 0; i < 5; i++)
+			{
+				for (int j = 0; j < 5; j++)
+				{
+					if (tmp[i][j] == 0) continue;
+					
+					int sx = nx + (j - 2);
+					int sy = ny + (i - 2);
+					int spread = amount * tmp[i][j]/100;
+					dust += spread;
+					if (IsValid(sx, sy))
+					{
+						vec[sy][sx] += spread;
+						
+					}
+					else
+					{
+						answer += spread;
+					}
+				}
+			}
+			int alpha = amount - dust;
+			vec[ny][nx] = 0;
+			int ax = nx + cur_dir.first;
+			int ay = ny + cur_dir.second;
+			if (IsValid(ax, ay)) vec[ay][ax] += alpha;
+			else answer += alpha;
+			cur_x = nx;
+			cur_y = ny;
+			if (cur_x == 0 && cur_y == 0)
+			{
+				cout << answer;
+				return 0;
+			}
+		}
+		idx = (idx + 1) % 4;
+		if (idx % 2 == 0) limit++;
+
+
+	}
+	return 0;
 }
