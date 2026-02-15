cdd345d2b1e0f2b698555215af85ab36ff297723
thddml <108908383+HongSongUi@users.noreply.github.com>
2026-02-15 19:11:18 +0900

    [Silver IV] Title: 동전 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.cc
+++ 백준/Silver/11047. 동전 0/동전 0.cc
@@ -1,29 +1,32 @@
 #include <bits/stdc++.h>
+#include <unordered_map>
 using namespace std;
 
 
 int main()
 {
 	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
-	
+	cin.tie(NULL);
+	cout.tie(NULL);
+
 	int n = 0, k = 0;
 	cin >> n >> k;
-	vector<int> arr(n);
+
+	vector<int> vec(n);
+
 	for (int i = 0; i < n; i++)
 	{
-		cin >> arr[i];
+		cin >> vec[i];
 	}
+
 	int answer = 0;
 	for (int i = n - 1; i >= 0; i--)
 	{
-		int cur_coin = arr[i];
-		if (k < cur_coin) continue;
-		while (k>=cur_coin)
-		{
-			k -= cur_coin;
-			answer++;
-		}
+		int money = vec[i];
+		if (money > k) continue;
+
+		answer += k / money;
+		k = k % money;
 	}
 	cout << answer;
 	return 0;
