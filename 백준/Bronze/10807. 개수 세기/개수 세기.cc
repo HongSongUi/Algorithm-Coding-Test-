60de0b032bc748243a0ad3141e28e5eaca01016e
thddml <108908383+HongSongUi@users.noreply.github.com>
2026-02-18 02:58:37 +0900

    [Bronze V] Title: 개수 세기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.cc
+++ 백준/Bronze/10807. 개수 세기/개수 세기.cc
@@ -1,29 +1,27 @@
 #include <bits/stdc++.h>
+#include <unordered_map>
 using namespace std;
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
 
-    int n = 0;
-    cin >> n;
-    map<int, int> temp;
-    int num = 0;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> num;
-        temp[num]++;
-    }
-    int k = 0;
-    cin >> k;
-    if (temp.find(k) == temp.end())
-    {
-        cout << 0;
-    }
-    else
-    {
-        cout << temp[k];
-    }
-    return 0;
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n = 0;
+	cin >> n;
+
+	map<int, int> tmp;
+
+	int num = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> num;
+		tmp[num]++;
+	}
+	cin >> num;
+	cout << tmp[num];
+	return 0;
 }
