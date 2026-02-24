51a48980d2507d0d9ef2800e49c335c307cc1ed1
thddml <108908383+HongSongUi@users.noreply.github.com>
2026-02-24 19:08:45 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2164 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -1,5 +1,8 @@
 #include <bits/stdc++.h>
-using namespace std;
+#include <unordered_map>
+
+using namespace std; 
+
 
 
 int main()
@@ -7,30 +10,29 @@ int main()
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-
+	
 	int n = 0;
 	cin >> n;
-	vector<vector<int>> arr(n, vector<int>(3,0));
-	vector<vector<int>> dp(n, vector<int>(3, 0));
+	vector<vector<int>> vec(n, vector<int>(3));
+	vector<vector<int>> dp(n, vector<int>(3));
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < 3; j++)
 		{
-			cin >> arr[i][j];
+			cin >> vec[i][j];
 		}
 	}
-	int idx = 0;
-	int answer = 0;
-	for (int i = 0; i < 3; i++)
-	{
-		dp[0][i] = arr[0][i];
-	}
+	dp[0][0] = vec[0][0];
+	dp[0][1] = vec[0][1];
+	dp[0][2] = vec[0][2];
+
 	for (int i = 1; i < n; i++)
 	{
-		dp[i][0] = arr[i][0] + min(dp[i - 1][1], dp[i - 1][2]);
-		dp[i][1] = arr[i][1] + min(dp[i - 1][0], dp[i - 1][2]);
-		dp[i][2] = arr[i][2] + min(dp[i - 1][0], dp[i - 1][1]);
+		dp[i][0] = vec[i][0] + min(dp[i - 1][1], dp[i - 1][2]);
+		dp[i][1] = vec[i][1] + min(dp[i - 1][0], dp[i - 1][2]);
+		dp[i][2] = vec[i][2] + min(dp[i - 1][0], dp[i - 1][1]);
 	}
+
 	cout << *min_element(dp[n - 1].begin(), dp[n - 1].end());
 	return 0;
 }
