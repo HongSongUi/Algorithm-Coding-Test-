3bfb6529c4aa8dd4d1cc12739e9f3f1edf2853db
thddml <108908383+HongSongUi@users.noreply.github.com>
2026-02-24 20:10:12 +0900

    [Silver I] Title: 정수 삼각형, Time: 8 ms, Memory: 4136 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -1,5 +1,8 @@
 #include <bits/stdc++.h>
-using namespace std;
+#include <unordered_map>
+
+using namespace std; 
+
 
 
 int main()
@@ -7,38 +10,39 @@ int main()
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-
+	
 	int n = 0;
 	cin >> n;
-	vector<vector<int>> arr(n);
+	vector<vector<int>>  vec(n,vector<int>(n));
+	vector<vector<int>> dp(n, vector<int>(n));
 	int num = 0;
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j <= i; j++)
 		{
-			cin >> num;
-			arr[i].emplace_back(num);
+			cin >> vec[i][j];		
 		}
 	}
-	vector<vector<int>> dp(n,vector<int>(n));
-	dp[0][0] = arr[0][0];
-	for (int i = 0; i < n-1; i++)
+	dp[0][0] = vec[0][0];
+	for (int i = 1; i < n; i++)
 	{
-		for (int j = 0; j <= i+1; j++)
+		for (int j = 0; j <= i; j++)
 		{
 			if (j == 0)
 			{
-				dp[i + 1][j] = arr[i+1][j] + dp[i][j];
-				continue;
+				dp[i][j] = vec[i][j] + dp[i - 1][j];
 			}
-			else if (j == i + 1)
+			else if (j == i)
 			{
-				dp[i + 1][j] = arr[i + 1][j] + dp[i][j-1];
-				continue;
+				dp[i][j] = vec[i][j] + dp[i - 1][j - 1];
 			}
-			dp[i + 1][j] =arr [i+1][j] + max(dp[i][j - 1], dp[i][j]);
+			else
+			{
+				dp[i][j] = vec[i][j] + max(dp[i - 1][j - 1], dp[i - 1][j]);
+			}
 		}
 	}
+
 	cout << *max_element(dp[n - 1].begin(), dp[n - 1].end());
 	return 0;
 }
