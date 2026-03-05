ce5a97dec1957354aba24f9f76c823d1c86e3468
thddml <108908383+HongSongUi@users.noreply.github.com>
2026-03-06 01:56:50 +0900

    [Silver I] Title: 효율적인 해킹, Time: 4392 ms, Memory: 3508 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
@@ -1,65 +1,64 @@
 #include <bits/stdc++.h>
-using namespace std;
+#include <unordered_map>
 
-int n = 0, m = 0;
+using namespace std; 
+
+int n , m;
 vector<vector<int>> graph;
-vector<vector<int>> tmp;
-int max_cnt = 0;
-
-
-
+vector<int> vec;
+int max_d = 0;
 void BFS(int p)
 {
-    vector<bool> visited(n + 1);
-    queue<pair<int, int>> q;
-    q.push(make_pair(p, 0));
-    visited[p] = true;
-    int cnt = 0;
-    while (!q.empty())
-    {
-        int cur = q.front().first;
-        int d = q.front().second;
-        q.pop();
+	vector<bool> visited(n + 1);
+	visited[p] = true;
+	queue<pair<int, int>> q;
+	q.push(make_pair(p, 0));
 
-        for (int i = 0; i < graph[cur].size(); i++)
-        {
-            int next = graph[cur][i];
-            if (!visited[next])
-            {
-                cnt++;
-                visited[next] = true;
-                q.push(make_pair(next, d + 1));
-            }
-        }
-    }
-    max_cnt = max(max_cnt, cnt);
-    tmp[cnt].emplace_back(p);
+	int d = 0;
+	while (!q.empty())
+	{
+		int curPoint = q.front().first;
+		int curDepth = q.front().second;
+		q.pop();
+		for (int i = 0; i < graph[curPoint].size(); i++)
+		{
+			int nextPoint = graph[curPoint][i];
+			if (!visited[nextPoint])
+			{
+				visited[nextPoint] = true;
+				q.push(make_pair(nextPoint, curDepth + 1));
+				d++;
+			}
+		}
+	}
+	max_d = max(max_d, d);
+	vec[p] = d;
 }
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    
-    cin >> n >> m;
-    graph.resize(n + 1);
-    tmp.resize(n + 1);
 
-    int s = 0;
-    int e = 0;
-    for (int i = 0; i < m; i++)
-    {
-        cin >> s >> e;
-        graph[e].emplace_back(s);
-    }
-    for (int i = 1; i <= n; i++)
-    {
-        BFS(i);
-    }
-    sort(tmp[max_cnt].begin(), tmp[max_cnt].end());
-    for (int i = 0; i < tmp[max_cnt].size(); i++)
-    {
-        cout << tmp[max_cnt][i] << ' ';
-    }
-    return 0;
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n >> m;
+
+	graph.resize(n + 1);
+	vec.resize(n + 1);
+	int s, e;
+	for (int i = 0; i < m; i++)
+	{
+		cin >> s >> e;
+		graph[e].emplace_back(s);
+	}
+	for (int i = 1; i <= n; i++)
+	{
+		BFS(i);
+	}
+	for (int i = 1; i < vec.size(); i++)
+	{
+		if (vec[i] == max_d) cout << i << ' ';
+	}
+	return 0;
 }
