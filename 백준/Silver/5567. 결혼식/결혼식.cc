4c902a72a4673ba1a6b96920edaa7f1669304188
thddml <108908383+HongSongUi@users.noreply.github.com>
2026-03-06 01:14:07 +0900

    [Silver II] Title: 결혼식, Time: 0 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Silver/5567. 결혼식/결혼식.cc
+++ 백준/Silver/5567. 결혼식/결혼식.cc
@@ -1,53 +1,66 @@
 #include <bits/stdc++.h>
-using namespace std;
+#include <unordered_map>
 
-int n = 0, m = 0;
+using namespace std; 
+
+int n, m;
 vector<vector<int>> graph;
+vector<bool> visited;
 
-int BFS()
+int bfs()
 {
-    vector<bool> visited(n + 1);
-    queue<pair<int,int>> q;
-    visited[1] = true;
-    q.push(make_pair(1, 0));
-    int ret = 0;
-    while (!q.empty())
-    {
-        int cur = q.front().first;
-        int d = q.front().second;
+	visited[1] = true;
+	queue<pair<int, int>> q;
+	int ret = 0;
+	q.push(make_pair(1, 0));
 
-        q.pop();
+	while (!q.empty())
+	{
+		int cur_node = q.front().first;
+		int cur_d = q.front().second;
+		if (cur_d >= 1 && cur_d <= 2)
+		{
+			ret++;
+		}
+		q.pop();
 
-        for (int i = 0; i < graph[cur].size(); i++)
-        {
-            int next = graph[cur][i];
-            if (!visited[next])
-            {
-                visited[next] = true;
-                if (d + 1 <= 2) ret++;
-                q.push(make_pair(next, d + 1));
-            }
-        }
-    }
-    return ret;
+		for (int i = 0; i < graph[cur_node].size(); i++)
+		{
+			int next_node = graph[cur_node][i];
+			if (visited[next_node] == false)
+			{
+				visited[next_node] = true;
+				q.push(make_pair(next_node, cur_d + 1));
+			
+			}
+		}
+	}
+	return ret;
 }
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    
 
-    cin >> n >> m;
-    graph.resize(n + 1);
-    int s = 0;
-    int e = 0;
-    for (int i = 0; i < m; i++)
-    {
-        cin >> s >> e;
-        graph[s].emplace_back(e);
-        graph[e].emplace_back(s);
-    }
-    cout << BFS();
-    return 0;
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n >> m;
+
+	graph.resize(n + 1);
+	visited.resize(n + 1);
+	int s, e;
+
+	for (int i = 0; i < m; i++)
+	{
+		cin >> s >> e;
+		graph[s].emplace_back(e);
+		graph[e].emplace_back(s);
+	}
+
+	int answer = bfs();
+	
+	cout << answer;
+
+	return 0;
 }
