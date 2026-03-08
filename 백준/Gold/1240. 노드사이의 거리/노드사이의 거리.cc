7381f8d72e25131109a9c0774aac781ebc9dfd2d
thddml <108908383+HongSongUi@users.noreply.github.com>
2026-03-09 00:50:05 +0900

    [Gold V] Title: 노드사이의 거리, Time: 12 ms, Memory: 2168 KB -BaekjoonHub

--- 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.cc
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.cc
@@ -1,66 +1,62 @@
 #include <bits/stdc++.h>
-using namespace std;
+#include <unordered_map>
 
-int n = 0, m = 0;
-vector<vector<pair<int, int>>> graph;
+using namespace std; 
+
+int n, m;
+
+vector<vector<pair<int,int>>> graph;
+
 int BFS(int s, int e)
 {
-    vector<bool> visited(n + 1, false);
-    queue<pair<int, int>> q;
-    q.push(make_pair(s, 0));
-    visited[s] = true;
-    int dist = 0;
-    while (!q.empty())
-    {
-        int cur_node = q.front().first;
-        int cur_dist = q.front().second;
+	vector<bool> visited(n + 1, false);
+	queue<pair<int, int>> q;
+	int dist = 0;
+	q.push(make_pair(s, 0));
 
-        if (cur_node == e)
-        {
-            dist = cur_dist;
-            break;
-        }
-        q.pop();
-        for (int i = 0; i < graph[cur_node].size(); i++)
-        {
-            int next_node = graph[cur_node][i].first;
-            int next_dist = cur_dist + graph[cur_node][i].second;
-            if (!visited[next_node])
-            {
-                visited[next_node] = true;
-                q.push(make_pair(next_node, next_dist));
-            }
-        }
-        
-    }
-    return dist;
+	while (!q.empty())
+	{
+		int cur_node = q.front().first;
+		int cur_dist = q.front().second;
+		if (cur_node == e)
+		{
+			return cur_dist;
+		}
+		q.pop();
+		for (int i = 0; i < graph[cur_node].size(); i++)
+		{
+			int next_node = graph[cur_node][i].first;
+			if (!visited[next_node])
+			{
+				visited[next_node] = true;
+				q.push(make_pair(next_node, cur_dist + graph[cur_node][i].second));
+			}
+		}
+	}
+
+	return dist;
 }
 
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    cin >> n >> m;
-    graph.resize(n + 1);
-
-    int dist = 0;
-    int node1 = 0;
-    int node2 = 0;
-    for (int i = 1; i < n; i++)
-    {
-        cin >> node1 >> node2 >> dist;
-        graph[node1].emplace_back(make_pair(node2, dist));
-        graph[node2].emplace_back(make_pair(node1, dist));
-    }
-
-    while (m--)
-    {
-        cin >> node1 >> node2;
-        
-        cout << BFS(node1, node2) << '\n';
-    }
-    
-    return 0;
+	int s, e, c;
+	cin >> n >> m;
+	graph.resize(n + 1);
+	for (int i = 0; i < n - 1; i++)
+	{
+		cin >> s >> e >> c;
+		graph[s].emplace_back(make_pair(e, c));
+		graph[e].emplace_back(make_pair(s, c));
+	}
+	while (m--)
+	{
+		cin >> s >> e;
+		cout << BFS(s, e) << '\n';
+	}
+	return 0;
 }
