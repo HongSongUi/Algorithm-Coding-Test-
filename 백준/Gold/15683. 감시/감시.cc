961c83a5fe882fb383f7c0d1ae9e0ad139119ebd
thddml <108908383+HongSongUi@users.noreply.github.com>
2026-03-11 02:29:27 +0900

    [Gold III] Title: 감시, Time: 40 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.cc
+++ 백준/Gold/15683. 감시/감시.cc
@@ -1,121 +1,171 @@
 #include <bits/stdc++.h>
-using namespace std;
+#include <unordered_map>
+
+using namespace std; 
 
 int n = 0, m = 0;
-vector<vector<int>> rooms;
-struct CCTV
-{
-    int num = 0;
-    int x = 0;
-    int y = 0;
-    int dir = 0;
-    CCTV()
-    {}
-    CCTV(int y, int x, int d,int n )
-    {
-        this->y = y;
-        this->x = x;
-        dir = d;
-        num = n;
-    }
-};
-vector<CCTV> cctvs;
-int dx[4] = { -1, 0, 1, 0 };
-int dy[4] = { 0, 1, 0, -1 };
-vector<vector<int>> cctvDir[6];
 
-// 초기화 함수
-void Init() 
+vector<pair<int, int>> dir = { {1,0},{0,1} , {-1,0} ,{0,-1} }; // x,y r,b,l,t
+vector<int> one_dir = { 0,1,2,3 };
+vector<vector<int>> two_dir = { {0,2},{1,3} };
+vector<vector<int>> three_dir = { {0,3},{0,1},{2,1},{2,3} };
+vector<vector<int>> four_dir = { {0,2,3},{3,0,1},{0,1,2},{2,1,3} };
+vector<vector<int>> five_dir = { {0,1,2,3} };
+
+struct cctv
 {
-    cctvDir[1] = { {0}, {1}, {2}, {3} };                   // Type 1
-    cctvDir[2] = { {0, 2}, {1, 3} };                        // Type 2
-    cctvDir[3] = { {0, 1}, {1, 2}, {2, 3}, {3, 0} };        // Type 3
-    cctvDir[4] = { {0, 1, 2}, {1, 2, 3}, {2, 3, 0}, {3, 0, 1} }; // Type 4
-    cctvDir[5] = { {0, 1, 2, 3} };                          // Type 5
-}
+	int x;
+	int y;
+	int num;
+	cctv()
+	{
+		x = 0;
+		y = 0;
+		num = 0;
+	}
+	cctv(int x, int y, int num)
+	{
+		this->num = num;
+		this->x = x;
+		this->y = y;
+	}
+};
+vector<cctv> cctvs;
 int answer = INT_MAX;
 bool IsValid(int x, int y)
 {
-    return (x >= 0 && x < m) && (y >= 0 && y < n);
+	return (x >= 0 && x < m) && (y >= 0 && y < n);
 }
-void MakeWall(int x, int y,vector<int>dir)
+void Vision(int x, int y, pair<int,int> dir,vector<vector<int>>& tmp)
 {
-    int cur_x = x;
-    int cur_y = y;
-    for (int i = 0; i < dir.size(); i++)
-    {
-        cur_x = x;
-        cur_y = y;
-        int tx = dx[dir[i]];
-        int ty = dy[dir[i]];
-        while (true)
-        {
-            int nx = tx + cur_x;
-            int ny = ty + cur_y;
-            if (!IsValid(nx , ny )) break;
-        
-            if (rooms[ny][nx] == 6)break;
-            rooms[ny][nx] = -1;
-            cur_x = nx;
-            cur_y = ny;
-        }
-    }
+	int cur_x = x;
+	int cur_y = y;
+	while (true)
+	{
+		int nx = cur_x + dir.first;
+		int ny = cur_y + dir.second;
+		if (!IsValid(nx, ny) || tmp[ny][nx] == 6) break;
+
+		if (tmp[ny][nx] == 0)
+		{
+			tmp[ny][nx] = -1;
+		}
+		cur_x = nx;
+		cur_y = ny;
+	}
+	return;
 }
-void Calc()
+void BackTracking(vector<vector<int>>& graph, int idx)
 {
-    int cnt = 0;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            if (rooms[i][j] == 0)cnt++;
-        }
-    }
-    answer = min(answer, cnt);
-}
-void Simulation(int cnt,int idx)
-{
-    if (idx == cctvs.size())
-    {
-        Calc();
-        return;
-    }
-    for (int i = idx; i < cctvs.size(); i++)
-    {
-        int num = cctvs[i].num;
-        for (int j = 0; j < cctvDir[num].size(); j++)
-        {
-            int x = cctvs[i].x;
-            int y = cctvs[i].y;
-            vector<int> tmp = cctvDir[num][j];
-            vector<vector<int>> before = rooms;
-            MakeWall(x, y, tmp);
-            Simulation(cnt + 1,i+1);
-            rooms = before;
-        }
-    }
-}
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+	if (idx >= cctvs.size())
+	{
+		int cnt = 0;
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < m; j++)
+			{
+				if (graph[i][j] == 0) cnt++;
+			}
+		}
+		answer = min(answer, cnt);
+		return;
+	}
+	
+		int num = cctvs[idx].num;
+		int x = cctvs[idx].x;
+		int y = cctvs[idx].y;
+		
+		if (num == 1)
+		{
+			for (int i = 0; i < 4; i++)
+			{
+				vector<vector<int>> tmp = graph;
+				int t = one_dir[i];
+				pair<int, int> d = dir[t];
+				Vision(x, y, d, tmp);
+				BackTracking(tmp, idx + 1);
+			}
+			
+		}
+		else if (num == 2)
+		{
+			for(int i = 0; i<2; i++)
+			{
+				vector<vector<int>> tmp = graph;
+				for (int j = 0; j < 2; j++)
+				{
+					int t = two_dir[i][j];
+					pair<int, int> d = dir[t];
+					Vision(x, y, d, tmp);
+				}
+				BackTracking(tmp, idx + 1);
+			}
+		}
+		else if (num == 3)
+		{
+			for (int i = 0; i < 4; i++)
+			{
+				vector<vector<int>> tmp = graph;
+				for (int j = 0; j < 2; j++)
+				{
+					int t = three_dir[i][j];
+					pair<int, int> d = dir[t];
+					Vision(x, y, d, tmp);
+				}
+				BackTracking(tmp, idx + 1);
+			}
+		}
+		else if (num == 4)
+		{
+			for (int i = 0; i < 4; i++)
+			{
+				vector<vector<int>> tmp = graph;
+				for (int j = 0; j < 3; j++)
+				{
+					int t = four_dir[i][j];
+					pair<int, int> d = dir[t];
+					Vision(x, y, d, tmp);
+				}
+				BackTracking(tmp, idx + 1);
+			}
+		}
+		else
+		{
+			vector<vector<int>> tmp = graph;
+			for (int i = 0; i < 4; i++)
+			{
+				int t = five_dir[0][i];
+				pair<int, int> d = dir[t];
+				Vision(x, y, d, tmp);
+			}
+			BackTracking(tmp, idx + 1);
+		}
 
 
-    cin >> n >> m;
-    rooms.resize(n, vector<int>(m));
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            cin >> rooms[i][j];
-            if (rooms[i][j] >= 1 && rooms[i][j] <= 5)
-            {
-                cctvs.emplace_back(CCTV{ i,j,0,rooms[i][j] });
-            }
-        }
-    }
-    Init();
-    Simulation(0,0);
-    cout << answer;
-    return 0;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n >> m;
+	vector<vector<int>> graph(n, vector<int>(m));
+	
+	int t = 0;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cin >> graph[i][j];
+			if (graph[i][j] > 0 && graph[i][j] < 6)
+			{
+				cctvs.emplace_back(cctv(j, i, graph[i][j]));
+			}
+		}
+	}
+	BackTracking(graph, 0);
+	cout << answer;
+	return 0;
 }
