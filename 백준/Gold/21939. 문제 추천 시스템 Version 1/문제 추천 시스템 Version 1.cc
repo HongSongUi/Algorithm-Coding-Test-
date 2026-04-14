ba5a9a1eae395cb3f2b98ce0df8337aee6047c35
thddml <108908383+HongSongUi@users.noreply.github.com>
2026-04-15 01:53:09 +0900

    [Gold IV] Title: 문제 추천 시스템 Version 1, Time: 68 ms, Memory: 11304 KB -BaekjoonHub

--- 백준/Gold/21939. 문제 추천 시스템 Version 1/문제 추천 시스템 Version 1.cc
+++ 백준/Gold/21939. 문제 추천 시스템 Version 1/문제 추천 시스템 Version 1.cc
@@ -1,64 +1,63 @@
 #include <bits/stdc++.h>
 #include <unordered_map>
+
 using namespace std;
 
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-        
-    int n = 0;
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    cin >> n;
- 
-    set<pair<int, int>> easySet;
-    set<pair<int, int>, greater<>> hardSet;
+	multiset<pair<int, int>> p_set;
+	unordered_map<int, int> p2l;
 
-    unordered_map<int, int> p2l;
+	int n = 0, m = 0;
+	cin >> n;
 
-    int p, l = 0;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> p >> l;
-        p2l[p] = l;
-        easySet.insert(make_pair(l, p));
-        hardSet.insert(make_pair(l, p));
-    }
-    int m = 0;
-    cin >> m;
-    string s = "";
-    int num = 0;
-    while (m--)
-    {
-        cin >> s;
-        if (s == "recommend")
-        {
-            cin >> num;
-            if (num == 1)
-            {
-                cout << hardSet.begin()->second << '\n';
-            }
-            else if (num == -1)
-            {
-                cout << easySet.begin()->second << '\n';
-            }
-        }
-        else if (s == "add")
-        {
-            cin >> p >> l;
-            p2l[p] = l;
-            easySet.insert(make_pair(l, p));
-            hardSet.insert(make_pair(l, p));
-        }
-        else if (s == "solved")
-        {
-            cin >> num;
-            int tmp = p2l[num];
-            p2l.erase(num);
-            easySet.erase(easySet.find(make_pair(tmp,num)));
-            hardSet.erase(hardSet.find(make_pair(tmp,num)));
-        }
-    }
-    return 0;
+	int p, l;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> p >> l;
+
+		p_set.insert(make_pair(l, p));
+		p2l.insert(make_pair(p, l));
+	}
+	cin >> m;
+	string s = "";
+	int x = 0;
+	while (m>0)
+	{
+		cin >> s;
+		if (s == "add")
+		{
+			cin >> p >> l;
+			p_set.insert(make_pair(l, p));
+			p2l.insert(make_pair(p, l));
+		}
+		else if (s == "recommend")
+		{
+			cin >> x;
+			if (x == 1)
+			{
+				cout << prev(p_set.end())->second << '\n';
+			}
+			else if (x == -1)
+			{
+				cout << p_set.begin()->second << '\n';
+			}
+		}
+		else if (s == "solved")
+		{
+			cin >> p;
+			auto it = p2l.find(p);
+			p_set.erase({ it->second, p });
+			p2l.erase(it);
+		}
+
+		m--;
+	}
+	
+	return 0;
 }
