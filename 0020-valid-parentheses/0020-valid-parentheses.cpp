8c62116b30fcf937d062dd35b471829d70fa3798
thddml <108908383+HongSongUi@users.noreply.github.com>
2026-08-19 01:29:47 +0900

    Time: 0 ms (100%), Space: 8.7 MB (98.45%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -3,33 +3,23 @@ public:
     bool isValid(string s) {
         stack<char> tmp;
 
-        	for (int i = 0; i < s.length(); i++)
-	{
-		char ch = s[i];
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s[i];
 
-		if (tmp.empty())
-		{
-			tmp.push(ch);
-			continue;
-		}
-		if (ch == ')' && tmp.top() == '(')
-		{
-			tmp.pop();
-		}
-		else if (ch == '}' && tmp.top() == '{')
-		{
-			tmp.pop();
-		}
-		else if (ch == ']' && tmp.top() == '[')
-		{
-			tmp.pop();
-		}
-		else
-		{
-			tmp.push(ch);
-		}
-
- 	}
+            if (tmp.empty()) {
+                tmp.push(ch);
+                continue;
+            }
+            if (ch == ')' && tmp.top() == '(') {
+                tmp.pop();
+            } else if (ch == '}' && tmp.top() == '{') {
+                tmp.pop();
+            } else if (ch == ']' && tmp.top() == '[') {
+                tmp.pop();
+            } else {
+                tmp.push(ch);
+            }
+        }
         return tmp.empty();
     }
 };
