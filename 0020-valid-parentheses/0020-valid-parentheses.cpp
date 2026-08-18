b5a42ba2550cbd061bd8f08dbe5832b6212b72cd
thddml <108908383+HongSongUi@users.noreply.github.com>
2026-08-19 01:29:23 +0900

    Time: 0 ms (100%), Space: 9.1 MB (18.13%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> tmp;
+
+        	for (int i = 0; i < s.length(); i++)
+	{
+		char ch = s[i];
+
+		if (tmp.empty())
+		{
+			tmp.push(ch);
+			continue;
+		}
+		if (ch == ')' && tmp.top() == '(')
+		{
+			tmp.pop();
+		}
+		else if (ch == '}' && tmp.top() == '{')
+		{
+			tmp.pop();
+		}
+		else if (ch == ']' && tmp.top() == '[')
+		{
+			tmp.pop();
+		}
+		else
+		{
+			tmp.push(ch);
+		}
+
+ 	}
+        return tmp.empty();
+    }
+};
